ea0afae7f2eb593497ce885d038ae3c05a20d9fc
Valter Silva <valter.silva.dev@gmail.com>
2023-09-07 21:57:29 -0300

    init: estrutura do projeto

---
+++ src/main/java/br/com/nesher/logisticsmanagement/LogisticsManagementApplication.java
@@ -0,0 +1,13 @@
+package br.com.nesher.logisticsmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticsManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/nesher/logisticsmanagement/LogisticsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nesher.logisticsmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
